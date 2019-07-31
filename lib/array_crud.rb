affe7d0024721db953aaad451f776f895700baa5
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-31 18:03:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Happy", "Sad", "Angry", "Sleepy"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.unshift()
 end
 
 def retrieve_element_from_index(array, index_number)
